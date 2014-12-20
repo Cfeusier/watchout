4688a71415139a8ecc5c39f518fe62e8fad6de78
Cfeusier <cfeusier@gmail.com>
2014-12-19 16:39:08 -0800

    Reorganize code into modular file structure

---
+++ enemy_collection.js
@@ -0,0 +1,13 @@
+var EnemyCollection = function() {
+  this.enemyList = [];
+};
+
+EnemyCollection.prototype = {
+  constructor: EnemyCollection,
+  generateEnemies: function(numOfEnemies, width, height) {
+    for (var i = 0; i< numOfEnemies; i++) {
+      this.enemyList.push(new Enemy(i, width, height))
+    }
+    return this.enemyList;
+  }
+};
---
+++ player.js
@@ -0,0 +1,20 @@
+var Player = function(width, height) {
+  // instantiate at the center of the board
+  this.x = width / 2;
+  this.y = height / 2;
+  this.color = "#0000ff";
+};
+
+Player.prototype = {
+  constructor: Player,
+  drag: function() {
+    d3.behavior.drag().on('drag', this.dragMove)
+      console.log("drag listener")
+  },
+  dragMove: function(d) {
+    console.log(this);
+    //d3.select(this)
+    //  .attr("cx", d.x = Math.max(15, Math.min(this.width - 15, d3.event.x)))
+    //  .attr("cy", d.y = Math.max(15, Math.min(this.height - 15, d3.event.y)));
+  }
+};
